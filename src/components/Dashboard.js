ec1d578a7b3bfba131123a9890b69325b6f11b72
JonasNon <officialjonasn@gmail.com>
2024-09-14 14:59:55 -0500

    components all have global state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     Container,
     Table,
@@ -8,10 +8,10 @@ import {
     TableRow
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
-import Chart from './Chart'
-import Total from './Total'
-import AddCar from './AddCar'
- 
+import Chart from '../containers/Chart'
+import Total from '../containers/Total'
+import AddCar from '../containers/AddCar'
+
 const Dashboard = (props) => {
     return (
         <Container maxWidth="lg" className="car-container">
@@ -44,7 +44,8 @@ const Dashboard = (props) => {
                         <TableCell>{car["horsepower"]}</TableCell>
                         <TableCell>
                             <DeleteIcon
-                                // add onClick method here
+                                onClick={() => props.removeCar(idx)}
+                                
                                 className="icon text-red" />
                         </TableCell>
                     </TableRow>
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,7 +4,20 @@ const user = (state = null) => state
 
 // add switch statements in here
 const cars = (state = [], action) => {
-    return state
+    
+    switch(action.type) {
+        case "ADD_CAR":
+          console.log([...state, action.value])
+          return [...state, action.value]
+        case "REMOVE_CAR":
+          console.log(state)
+          let tempArr = [...state]
+          tempArr.splice(action.value, 1)
+          console.log(tempArr)
+          return tempArr
+        default:
+          return state
+    }
 }
 
 export default combineReducers({ user, cars })
